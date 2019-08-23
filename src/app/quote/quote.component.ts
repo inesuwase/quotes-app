bb31e95e83305181597afebc4718eddce2407c2d
ines uwase <uwaseines10@gmail.com>
2019-08-23 17:32:38 +0200

    i have added the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import{Quote} from './quote'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class AppComponent {
+export class QuoteComponent implements OnInit {
   quotes:Quote []= [
     {id:1, name:'Til your good is better and your better is best'},
     {id:2,name:'You learn more from failure than from success.'},
     {id:3,name:'The pessimist sees difficulty in every opportunity.'},
     {id:4,name:'Life is not about finding yourself.'},
   ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
